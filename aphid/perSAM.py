b484088cc69c188b15d4fa8b08a57335d249c6c6
underasail <underasail@yahoo.com>
2018-02-16 12:44:17 -0500

    Update mismatches comment

--- aphid/perSAM.py
+++ aphid/perSAM.py
@@ -45,7 +45,7 @@ with open(argv[1], newline='') as f:
     csvreader = csv.reader(f, delimiter = '\t')
     for row in csvreader:
         if (len(row) >= 14) and ('XM:i:0' or 'XM:i:1' in str(row)):
-            # used to select only for allignments with no mismatches
+            # used to select only for allignments with zero or one mismatches
             # length factor skips header rows also
             readnum = row[0]
             refgen = row[2]
