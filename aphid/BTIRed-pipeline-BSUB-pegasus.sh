99f3daf5ce3d15f76289591a15b4101d5e212124
underasail <underasail@yahoo.com>
2018-02-16 15:06:11 -0500

    Swapped G002 for BTIRed

--- aphid/BTIRed-pipeline-BSUB-pegasus.sh
+++ aphid/BTIRed-pipeline-BSUB-pegasus.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-#BSUB -J G002_pipline
-#BSUB -e /nethome/mct30/err/G002_pipline.err
-#BSUB -o /nethome/mct30/out/G002_pipline.out
+#BSUB -J BTIRed_pipeline
+#BSUB -e /nethome/mct30/err/BTIRed_pipline.err
+#BSUB -o /nethome/mct30/out/BTIRed_pipline.out
 #BSUB -n 8
 #BSUB -q general
 #BSUB -W 72:00
@@ -22,55 +22,55 @@ module load bowtie2
 # need to load bowtie2 from /share
 
 
-###########################################
-# G002 Myzus/Buchnera Together Alignments #
-###########################################
+#############################################
+# BTIRed Myzus/Buchnera Together Alignments #
+#############################################
 
 /share/apps/bowtie2/2.2.6/bowtie2-build -f \
-/nethome/mct30/bmds/ref_genomes/G002_Buchnera_genome_ref.fasta,\
-/nethome/mct30/bmds/ref_genomes/G002_Buchnera_pLeu_ref.fasta,\
+/nethome/mct30/bmds/ref_genomes/BTIRed_Buchnera_genome_ref.fasta,\
+/nethome/mct30/bmds/ref_genomes/BTIRed_Buchnera_pLeu_ref.fasta,\
 /nethome/mct30/bmds/ref_genomes/G006_Myzus_genome_ref.fasta \
-/nethome/mct30/bmds/index/G002/G002_Buchnera_Myzus_index
-# Building an index for the G002 line using the genome and plasmid sections of the G002 genome
+/nethome/mct30/bmds/index/BTIRed/BTIRed_Buchnera_Myzus_index
+# Building an index for the BTIRed line using the genome and plasmid sections of the BTIRed genome
 # as well as the G006 Myzus genome
 
 /share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
--x /nethome/mct30/bmds/index/G002/G002_Buchnera_Myzus_index \
--U /nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35.fa \
--S /nethome/mct30/bmds/SAM_out/G002_Bac_Buchnera.map
-# G002 bacteriocyte reads aligned against G002 Buchnera (genome and plasmid)
+-x /nethome/mct30/bmds/index/BTIRed/BTIRed_Buchnera_Myzus_index \
+-U /nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35.fa \
+-S /nethome/mct30/bmds/SAM_out/BTIRed_Bac_Buchnera.map
+# BTIRed bacteriocyte reads aligned against BTIRed Buchnera (genome and plasmid)
 # and G006 Myzus (genome)
 
 /share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
--x /nethome/mct30/bmds/index/G002/G002_Buchnera_Myzus_index \
--U /nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35.fa \
--S /nethome/mct30/bmds/SAM_out/G002_Gut_Buchnera.map
-# G002 gut reads aligned against G002 Buchnera (genome and plasmid)
+-x /nethome/mct30/bmds/index/BTIRed/BTIRed_Buchnera_Myzus_index \
+-U /nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35.fa \
+-S /nethome/mct30/bmds/SAM_out/BTIRed_Gut_Buchnera.map
+# BTIRed gut reads aligned against BTIRed Buchnera (genome and plasmid)
 # and G006 Myzus (genome)
 
 
-#################################
-# G002 Unmatched FASTA Creation #
-#################################
+###################################
+# BTIRed Unmatched FASTA Creation #
+###################################
 
 /nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
-/nethome/mct30/bmds/SAM_out/G002_Bac_Buchnera.map \
-/nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35.fa \
-/nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35_unmatched.fasta
-# Builds new FASTA file for G002 bacteriocyte reads that didn't
+/nethome/mct30/bmds/SAM_out/BTIRed_Bac_Buchnera.map \
+/nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35.fa \
+/nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35_unmatched.fasta
+# Builds new FASTA file for BTIRed bacteriocyte reads that didn't
 # align to reference genomes for Buchnera and/or Myzus
 
 /nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
-/nethome/mct30/bmds/SAM_out/G002_Gut_Buchnera.map \
-/nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35.fa \
-/nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35_unmatched.fasta
-# Builds new FASTA file for G002 gut reads that didn't perfectly
+/nethome/mct30/bmds/SAM_out/BTIRed_Gut_Buchnera.map \
+/nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35.fa \
+/nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35_unmatched.fasta
+# Builds new FASTA file for BTIRed gut reads that didn't perfectly
 # align to reference genomes for Buchnera and/or Myzus
 
 
-##########################
-# G002 Plants Alignments #
-##########################
+############################
+# BTIRed Plants Alignments #
+############################
 
 export filelist=''
 for filename in /nethome/mct30/bmds/ref_genomes/plants/*.fasta; do
@@ -85,20 +85,20 @@ $filelist \
 
 /share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
 -x /nethome/mct30/bmds/index/plants/plants_index \
--U /nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35_unmatched.fasta \
--S /nethome/mct30/bmds/SAM_out/G002_Bac_plants.map
-# G002 bacteriocyte reads aligned against the plants
+-U /nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35_unmatched.fasta \
+-S /nethome/mct30/bmds/SAM_out/BTIRed_Bac_plants.map
+# BTIRed bacteriocyte reads aligned against the plants
 
 /share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
 -x /nethome/mct30/bmds/index/plants/plants_index \
--U /nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35_unmatched.fasta \
--S /nethome/mct30/bmds/SAM_out/G002_Gut_plants.map
-# G002 gut reads aligned against the plants
+-U /nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35_unmatched.fasta \
+-S /nethome/mct30/bmds/SAM_out/BTIRed_Gut_plants.map
+# BTIRed gut reads aligned against the plants
 
 
-##################################
-# G002 Other Bacteria Alignments #
-##################################
+####################################
+# BTIRed Other Bacteria Alignments #
+####################################
 
 export filelist=''
 for filename in /nethome/mct30/bmds/ref_genomes/other_bacteria/*.fasta; do
@@ -113,20 +113,20 @@ $filelist \
 
 /share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
 -x /nethome/mct30/bmds/index/other_bacteria/other_bacteria_index \
--U /nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35_unmatched.fasta \
--S /nethome/mct30/bmds/SAM_out/G002_Bac_other_bacteria.map
-# G002 bacteriocyte reads aligned against the other_bacteria
+-U /nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35_unmatched.fasta \
+-S /nethome/mct30/bmds/SAM_out/BTIRed_Bac_other_bacteria.map
+# BTIRed bacteriocyte reads aligned against the other_bacteria
 
 /share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
 -x /nethome/mct30/bmds/index/other_bacteria/other_bacteria_index \
--U /nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35_unmatched.fasta \
--S /nethome/mct30/bmds/SAM_out/G002_Gut_other_bacteria.map
-# G002 gut reads aligned against the other_bacteria
+-U /nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35_unmatched.fasta \
+-S /nethome/mct30/bmds/SAM_out/BTIRed_Gut_other_bacteria.map
+# BTIRed gut reads aligned against the other_bacteria
 
 
-###########################
-# G002 Viruses Alignments #
-###########################
+#############################
+# BTIRed Viruses Alignments #
+#############################
 
 export filelist=''
 for filename in /nethome/mct30/bmds/ref_genomes/viruses/*.fasta; do
@@ -141,23 +141,23 @@ $filelist \
 
 /share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
 -x /nethome/mct30/bmds/index/viruses/viruses_index \
--U /nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35_unmatched.fasta \
--S /nethome/mct30/bmds/SAM_out/G002_Bac_viruses.map
-# G002 bacteriocyte reads aligned against the viruses
+-U /nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35_unmatched.fasta \
+-S /nethome/mct30/bmds/SAM_out/BTIRed_Bac_viruses.map
+# BTIRed bacteriocyte reads aligned against the viruses
 
 /share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
 -x /nethome/mct30/bmds/index/viruses/viruses_index \
--U /nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35_unmatched.fasta \
--S /nethome/mct30/bmds/SAM_out/G002_Gut_viruses.map
-# G002 gut reads aligned against the viruses
+-U /nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35_unmatched.fasta \
+-S /nethome/mct30/bmds/SAM_out/BTIRed_Gut_viruses.map
+# BTIRed gut reads aligned against the viruses
 
 
-################################
-# G002 Percentage Calculations #
-################################
+##################################
+# BTIRed Percentage Calculations #
+##################################
 
 cd /nethome/mct30/bmds/SAM_out/
-for filename in G002*Buchnera.map; do
+for filename in BTIRed*Buchnera.map; do
     /nethome/mct30/gitclones/bmds/aphid/perSAM.py \
     /nethome/mct30/bmds/SAM_out/$filename \
     /nethome/mct30/gitclones/bmds/aphid/files/$filename.tsv
@@ -165,11 +165,11 @@ done
 # Gets percentage of matched reads for Buchnera and Myzus genomes
 
 
-##############################################
-# Percentage for G002 Other Bacteria Genomes #
-##############################################
+################################################
+# Percentage for BTIRed Other Bacteria Genomes #
+################################################
 
-for filename in G002*other_bacteria.map; do
+for filename in BTIRed*other_bacteria.map; do
     /nethome/mct30/gitclones/bmds/aphid/perGenomes.py \
     /nethome/mct30/bmds/SAM_out/$filename \
     /nethome/mct30/gitclones/bmds/aphid/files/$filename.tsv
