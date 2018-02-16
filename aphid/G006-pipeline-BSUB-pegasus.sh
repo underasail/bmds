bd9498680b88315c836ae2585aa41cd864986926
underasail <underasail@yahoo.com>
2018-02-16 12:20:22 -0500

    Cleaned up titles

--- aphid/G006-pipeline-BSUB-pegasus.sh
+++ aphid/G006-pipeline-BSUB-pegasus.sh
@@ -22,9 +22,36 @@ module load bowtie2
 # need to load bowtie2 from /share
 
 
-#
-# G006 Unmatched FASTA Creation
-#
+###########################################
+# G006 Myzus/Buchnera Together Alignments #
+###########################################
+
+/share/apps/bowtie2/2.2.6/bowtie2-build -f \
+/nethome/mct30/bmds/ref_genomes/G006_Buchnera_1genome_ref.fasta,\
+/nethome/mct30/bmds/ref_genomes/G006_Buchnera_2genome_ref.fasta,\
+/nethome/mct30/bmds/ref_genomes/G006_Myzus_genome_ref.fasta \
+/nethome/mct30/bmds/index/G006/G006_Buchnera_Myzus_index
+# Building an index for the G006 line using the two sections of the G006 genome
+# as well as the G006 Myzus genome
+
+/share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
+-x /nethome/mct30/bmds/index/G006/G006_Buchnera_Myzus_index \
+-U /nethome/mct30/bmds/reads/G006_Bac_F_trimmed_17-35.fa \
+-S /nethome/mct30/bmds/SAM_out/G006_Bac_Buchnera.map
+# G006 bacteriocyte reads aligned against G006 Buchnera (genome parts 1 and 2)
+# and G006 Myzus (genome)
+
+/share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal \
+-x /nethome/mct30/bmds/index/G006/G006_Buchnera_Myzus_index \
+-U /nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35.fa \
+-S /nethome/mct30/bmds/SAM_out/G006_Gut_Buchnera.map
+# G006 gut reads aligned against G006 Buchnera (genome parts 1 and 2)
+# and G006 Myzus (genome)
+
+
+#################################
+# G006 Unmatched FASTA Creation #
+#################################
 
 /nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
 /nethome/mct30/bmds/SAM_out/G006_Bac_Buchnera.map \
@@ -41,10 +68,9 @@ module load bowtie2
 # align to reference genomes for Buchnera and/or Myzus
 
 
-
-#
-# G006 Plants Alignments
-#
+##########################
+# G006 Plants Alignments #
+##########################
 
 export filelist=''
 for filename in /nethome/mct30/bmds/ref_genomes/plants/*.fasta; do
@@ -70,9 +96,9 @@ $filelist \
 # G006 gut reads aligned against the plants
 
 
-#
-# G006 Other Bacteria Alignments
-#
+##################################
+# G006 Other Bacteria Alignments #
+##################################
 
 export filelist=''
 for filename in /nethome/mct30/bmds/ref_genomes/other_bacteria/*.fasta; do
@@ -98,9 +124,9 @@ $filelist \
 # G006 gut reads aligned against the other_bacteria
 
 
-#
-# G006 Viruses Alignments
-#
+###########################
+# G006 Viruses Alignments #
+###########################
 
 export filelist=''
 for filename in /nethome/mct30/bmds/ref_genomes/viruses/*.fasta; do
@@ -126,11 +152,9 @@ $filelist \
 # G006 gut reads aligned against the viruses
 
 
-
-
-#
-# G006 Percentage Calculations
-#
+################################
+# G006 Percentage Calculations #
+################################
 
 for filename in /nethome/mct30/bmds/SAM_out/G006*.map; do
     /nethome/mct30/gitclones/bmds/aphid/perSAM.py \
