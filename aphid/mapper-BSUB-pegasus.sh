ddd0c415d4339e511b617125e29b63ba65d4d65f
underasail <underasail@yahoo.com>
2018-03-27 11:10:55 -0400

    Updated mapper script

---
+++ aphid/mapper-BSUB-pegasus.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+#BSUB -J mapper
+#BSUB -e /nethome/mct30/err/mapper.err
+#BSUB -o /nethome/mct30/out/mapper.out
+#BSUB -n 1
+#BSUB -q general
+#BSUB -W 24:00
+#BSUB -B
+#BSUB -N
+#BSUB -u mct30@miami.edu
+#
+# Job title, error output, standard output, number of cores,
+# queue, run time limit, send email when jobs begins, 
+# send email with stats when job finished, email,
+# default RAM per core is 1500MB
+
+cd /nethome/mct30/gitclones/mirdeep2/
+
+/nethome/mct30/gitclones/mirdeep2/bin/mapper.pl \
+/nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35.fa -c \
+-p /nethome/mct30/bmds/index/bt1-G006-Myzus/bt1-G006-Myzus-index \
+-q -t /nethome/mct30/bmds/G006_Gut_mapped.arf
+# Input FASTA file (-c) of reads
+# -p 'path to genome index'
+# -q with one mismatch
+# -t output mapped arf file
