7ce6c7b659e5ae40855757f2d07783ad95c5da96
underasail <underasail@yahoo.com>
2018-04-11 20:22:13 -0400

    Updated

--- structure/check-status-mem-ab.sh
+++ structure/check-status-mem-ab.sh
@@ -8,21 +8,27 @@ echo -n '# '
 ls -lh ~/aphid/mem-ab-out.*/*.pdb | wc -l 
 echo '# Number of cores running:                   #'
 echo -n '# '
-bjobs | grep 'RUN' | wc -l
-max=0
-for filename in ~/aphid/mem-ab-out.*; do 
-    # echo $filename; 
-    number=$(ls $filename | tail -n 1); 
-    number=${number:7:3}; 
-    # echo $number;
-    if [ $number -gt $max ]; then
-	max=$number;
-	maxfile=$filename
-    fi
-done
-echo '# Script with max output has created:        #'
-echo -n '# '
-echo -n $max
-echo ' structures'
-echo $maxfile
+bjobs | grep 'allel' | wc -l
+#max='0'
+#for filename in ~/aphid/mem-ab-out.*; do 
+#    # echo $filename; 
+#    number=$(ls $filename | tail -n 1); 
+#    number=${number:7:3}; 
+#    # echo $number;
+#    if [ ${number:0:2} == 00 ]; then
+#	number=${number:2:1};
+#    elif [ ${number:0:1} == 0 ]; then
+#	number=${number:1:2}
+#    fi
+#    # echo $number 
+#    if [ $number -gt $max ]; then
+#	max=$number;
+#	maxfile=$filename;
+#    fi
+#done
+#echo '# Script with max output has created:        #'
+#echo -n '# '
+#echo -n $max
+#echo ' structures'
+#echo $maxfile
 echo '##############################################'
