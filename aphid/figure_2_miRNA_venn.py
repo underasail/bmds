c30c26b2842315a7b07876908f61731db31aa0c3
underasail <underasail@yahoo.com>
2018-05-11 12:47:30 -0400

    Added figure 2 venn diagram script

---
+++ aphid/figure_2_miRNA_venn.py
@@ -0,0 +1,34 @@
+from matplotlib_venn import venn3
+from matplotlib import pyplot as plt
+
+plt.figure(figsize = (5, 5))
+venn3(subsets = (69, 93, 43, 103, 25, 40, 24), 
+      set_labels = ('$\it{M. persicae}$, current', 
+                    '$\it{M. persicae}$, prior', 
+                    '$\it{A. pisum}$, prior')
+      )
+
+#      Set1                      Set2
+# +-------------+-----------+-------------+
+# |             |           |             |
+# |             |           |             |
+# |     1       |     3     |       2     |
+# |             |           |             |
+# |       +----------------------+        |
+# |       |     |           |    |        |
+# |       |  5  |     7     |  6 |        |
+# |       |     |           |    |        |
+# +-------------+-----------+-------------+
+#         |                      |
+#         |                      |
+#         |           4          |
+#         |                      |
+#         |                      |
+#         +----------------------+
+#                   Set3
+
+
+
+plt.savefig('C:\\Users\Thompson\Documents\Figure_2_miRNA_Venn.svg', 
+            bbox_inches = 'tight', format = 'svg')
+plt.show()
