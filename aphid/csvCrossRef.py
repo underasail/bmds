d1cb10d20ff595c140ae2e768d59cdcdc4511b45
underasail <underasail@yahoo.com>
2018-05-11 11:59:41 -0400

    Fixed final count method using set

--- aphid/csvCrossRef.py
+++ aphid/csvCrossRef.py
@@ -3,19 +3,19 @@
 import csv
 
 compdict = {}
-i = 0
+final = []
 
-with open('C:\\Users\Thompson\Documents\mpe.csv', 
-          newline = '') as fone,  open('C:\\Users\Thompson\Documents\pred.csv', 
-          newline = '') as ftwo:
+with open('C:\\Users\Thompson\Documents\mpe.csv') as fone, \
+     open('C:\\Users\Thompson\Documents\pred.csv') as ftwo:
     onecsv = csv.reader(fone, delimiter = ',')
     twocsv = csv.reader(ftwo, delimiter = ',')
-    oneheader = next(onecsv)
-    twoheader = next(twocsv)
+    # oneheader = next(onecsv)
+    # twoheader = next(twocsv)
     for onerow in onecsv:
         compdict[onerow[0]] = onerow
     for tworow in twocsv:
         if tworow[0] in compdict:
             print(tworow[0])
-            i+=1
-print(i)
+            final.append(tworow[0])
+final = set(final)
+print(len(final))
