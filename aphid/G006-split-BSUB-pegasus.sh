8ab58e8dfa77c3ad82de01b34e5cd6cce10a2f4f
underasail <underasail@yahoo.com>
2018-05-29 15:30:59 -0400

    Added all together alignments

--- aphid/G006-split-BSUB-pegasus.sh
+++ aphid/G006-split-BSUB-pegasus.sh
@@ -3,7 +3,7 @@
 #BSUB -J G006-bowtie2_Buch_Myz_plant_split_alignments
 #BSUB -e /nethome/mct30/err/G006-bowtie2_Buch_Myz_plant_split_alignments.err
 #BSUB -o /nethome/mct30/out/G006-bowtie2_Buch_Myz_plant_split_alignments.out
-#BSUB -n 48
+#BSUB -n 16
 #BSUB -P acypi
 #BSUB -R "span[ptile=16]"
 #BSUB -q parallel
@@ -25,15 +25,23 @@ module load bowtie2
 # Myzus, Buchnera, and Plant Individually  #
 ############################################
 
-parallel --link -j 6 \
+# parallel --link -j 6 \
+# 'bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
+# -x /nethome/mct30/bmds/index/{1}/{1}_index \
+# -U /nethome/mct30/bmds/reads/{2} \
+# -S /nethome/mct30/bmds/SAM_out/{3}' \
+# ::: G006-Myzus G006-Myzus G006-Buchnera G006-Buchnera plants plants \
+# ::: G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
+# G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
+# G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
+# ::: G006_Bac_Myzus-only.map G006_Gut_Myzus-only.map \
+# G006_Bac_Buchnera-only.map G006_Gut_Buchnera-only.map \
+# G006_Bac_plants-only.map G006_Gut_plants-only.map
+
+parallel --link -j 2 \
 'bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
--x /nethome/mct30/bmds/index/{1}/{1}_index \
--U /nethome/mct30/bmds/reads/{2} \
--S /nethome/mct30/bmds/SAM_out/{3}' \
-::: G006-Myzus G006-Myzus G006-Buchnera G006-Buchnera plants plants \
+-x /nethome/mct30/bmds/index/G006/G006_Buchnera_Myzus_plant_index \
+-U /nethome/mct30/bmds/reads/{1} \
+-S /nethome/mct30/bmds/SAM_out/{2}' \
 ::: G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
-G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
-G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
-::: G006_Bac_Myzus-only.map G006_Gut_Myzus-only.map \
-G006_Bac_Buchnera-only.map G006_Gut_Buchnera-only.map \
-G006_Bac_plants-only.map G006_Gut_plants-only.map
+::: G006_Bac_Buchnera_Myzus_plant.map G006_Gut_Buchnera_Myzus_plant.map
