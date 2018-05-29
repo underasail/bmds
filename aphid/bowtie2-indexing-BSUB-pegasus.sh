5f4d14f2a7738508cf8b491480d3d2ee479835f8
underasail <underasail@yahoo.com>
2018-05-29 17:04:24 -0400

    Updated

--- aphid/bowtie2-indexing-BSUB-pegasus.sh
+++ aphid/bowtie2-indexing-BSUB-pegasus.sh
@@ -3,10 +3,11 @@
 #BSUB -J bowtie2-indexing
 #BSUB -e /scratch/projects/acypi/err/bowtie2-indexing.err
 #BSUB -o /scratch/projects/acypi/out/bowtie2-indexing.out
-#BSUB -n 7
+#BSUB -n 3
 #BSUB -P acypi
-#BSUB -q general
-#BSUB -W 24:00
+#BSUB -q bigmem
+#BSUB -R "rusage[mem=2000]"
+#BSUB -W 2:00
 #BSUB -B
 #BSUB -N
 #BSUB -u mct30@miami.edu
@@ -18,22 +19,23 @@
 
 module load bowtie2
 
+#parallel --link \
+#'export filelist=
+#for filename in /nethome/mct30/bmds/ref_genomes/{}/*.fasta; do
+#    filelist+=$filename;
+#    filelist+=",";
+#done
+#
+#bowtie2-build -f \
+#$filelist \
+#/nethome/mct30/bmds/index/{}/{}_index' \
+#::: plants viruses other_bacteria G006-Myzus G006-Buchnera \
+#G002-Buchnera BTIRed-Buchnera
+
 parallel --link \
-'export filelist=
-for filename in /nethome/mct30/bmds/ref_genomes/{}/*.fasta; do
-    filelist+=$filename;
-    filelist+=",";
-done
-
-bowtie2-build -f \
-$filelist \
-/nethome/mct30/bmds/index/{}/{}_index' \
-::: plants viruses other_bacteria G006-Myzus G006-Buchnera \
-G002-Buchnera BTIRed-Buchnera
-
-# parallel --link \
-# "bowtie2-build -f \
-# /nethome/mct30/bmds/ref_genomes/{}-Buchnera/{}_Buchnera_genome_ref.fasta,\
-# /nethome/mct30/bmds/ref_genomes/G006-Myzus/G006_Myzus_genome_ref.fasta \
-# /nethome/mct30/bmds/index/{}/{}_Buchnera_Myzus_index" \
-# ::: G002 G006 BTIRed
+"bowtie2-build -f \
+/nethome/mct30/bmds/ref_genomes/{}-Buchnera/{}_Buchnera_genome_ref.fasta,\
+/nethome/mct30/bmds/ref_genomes/G006-Myzus/G006_Myzus_genome_ref.fasta,\
+/nethome/mct30/bmds/ref_genomes/plants/GCA_000604025.1_BOL_v1.0_genomic.fasta \
+/nethome/mct30/bmds/index/{}/{}_Buchnera_Myzus_plant_index" \
+::: G002 G006 BTIRed
--- aphid/elimMatched-BSUB-pegasus.sh
+++ aphid/elimMatched-BSUB-pegasus.sh
@@ -3,9 +3,11 @@
 #BSUB -J eliminate_matched
 #BSUB -e /nethome/mct30/eliminate_matched.err
 #BSUB -o /nethome/mct30/eliminate_matched.out
-#BSUB -n 1
+#BSUB -n 2
+#BSUB -P acypi
+#BSUB -R "span[ptile=16]"
 #BSUB -R "rusage[mem=6500]"
-#BSUB -q general
+#BSUB -q bigmem
 #BSUB -W 72:00
 #BSUB -B
 #BSUB -N
@@ -16,62 +18,71 @@
 # send email with stats when job finished, email,
 # default RAM per core is 1500MB
 
-module switch python/3.3.1
+module switch python/3.6.5
 # need to work in python3
 
 
-#
-# G002
-#
+# #
+# # G002
+# #
 
-/nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
-/nethome/mct30/bmds/SAM_out/G002_Bac_Buchnera.map \
-/nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35.fa \
-/nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35_unmatched.fasta
-# Builds new FASTA file for G002 bacteriocyte reads that didn't perfectly
-# align to reference genomes for Buchnera and/or Myzus
+# /nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
+# /nethome/mct30/bmds/SAM_out/G002_Bac_Buchnera.map \
+# /nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35.fa \
+# /nethome/mct30/bmds/reads/G002_Bac_trimmed_17-35_unmatched.fasta
+# # Builds new FASTA file for G002 bacteriocyte reads that didn't perfectly
+# # align to reference genomes for Buchnera and/or Myzus
 
-/nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
-/nethome/mct30/bmds/SAM_out/G002_Gut_Buchnera.map \
-/nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35.fa \
-/nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35_unmatched.fasta
-# Builds new FASTA file for G002 gut reads that didn't perfectly
-# align to reference genomes for Buchnera and/or Myzus
+# /nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
+# /nethome/mct30/bmds/SAM_out/G002_Gut_Buchnera.map \
+# /nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35.fa \
+# /nethome/mct30/bmds/reads/G002_Gut_trimmed_17-35_unmatched.fasta
+# # Builds new FASTA file for G002 gut reads that didn't perfectly
+# # align to reference genomes for Buchnera and/or Myzus
 
 
 #
 # G006
 #
 
-/nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
-/nethome/mct30/bmds/SAM_out/G006_Bac_Buchnera.map \
-/nethome/mct30/bmds/reads/G006_Bac_F_trimmed_17-35.fa \
-/nethome/mct30/bmds/reads/G006_Bac_F_trimmed_17-35_unmatched.fasta
-# Builds new FASTA file for G006 bacteriocyte reads that didn't perfectly
-# align to reference genomes for Buchnera and/or Myzus
+parallel --link -j 2 \
+'/nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
+/nethome/mct30/bmds/SAM_out/{1} \
+/nethome/mct30/bmds/reads/{2} \
+/nethome/mct30/bmds/reads/{3}' \
+::: G006_Bac_Buchnera_Myzus_plant.map G006_Gut_Buchnera_Myzus_plant.map \
+::: G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
+::: G006_Bac_F_trimmed_17-35_unmatched.fasta G006_Gut_F_trimmed_17-35_unmatched.fasta
 
-/nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
-/nethome/mct30/bmds/SAM_out/G006_Gut_Buchnera.map \
-/nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35.fa \
-/nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35_unmatched.fasta
-# Builds new FASTA file for G006 gut reads that didn't perfectly
-# align to reference genomes for Buchnera and/or Myzus
+# /nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
+# /nethome/mct30/bmds/SAM_out/G006_Bac_Buchnera.map \
+# /nethome/mct30/bmds/reads/G006_Bac_F_trimmed_17-35.fa \
+# /nethome/mct30/bmds/reads/G006_Bac_F_trimmed_17-35_unmatched.fasta
+# # Builds new FASTA file for G006 bacteriocyte reads that didn't perfectly
+# # align to reference genomes for Buchnera and/or Myzus
+
+# /nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
+# /nethome/mct30/bmds/SAM_out/G006_Gut_Buchnera.map \
+# /nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35.fa \
+# /nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35_unmatched.fasta
+# # Builds new FASTA file for G006 gut reads that didn't perfectly
+# # align to reference genomes for Buchnera and/or Myzus
 
 
-#
-# BTIRed
-#
+# #
+# # BTIRed
+# #
 
-/nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
-/nethome/mct30/bmds/SAM_out/BTIRed_Bac_Buchnera.map \
-/nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35.fa \
-/nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35_unmatched.fasta
-# Builds new FASTA file for BTIRed bacteriocyte reads that didn't perfectly
-# align to reference genomes for Buchnera and/or Myzus
+# /nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
+# /nethome/mct30/bmds/SAM_out/BTIRed_Bac_Buchnera.map \
+# /nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35.fa \
+# /nethome/mct30/bmds/reads/BTIRed_Bac_trimmed_17-35_unmatched.fasta
+# # Builds new FASTA file for BTIRed bacteriocyte reads that didn't perfectly
+# # align to reference genomes for Buchnera and/or Myzus
 
-/nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
-/nethome/mct30/bmds/SAM_out/BTIRed_Gut_Buchnera.map \
-/nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35.fa \
-/nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35_unmatched.fasta
-# Builds new FASTA file for BTIRed gut reads that didn't perfectly
-# align to reference genomes for Buchnera and/or Myzus
+# /nethome/mct30/gitclones/bmds/aphid/elimMatched.py \
+# /nethome/mct30/bmds/SAM_out/BTIRed_Gut_Buchnera.map \
+# /nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35.fa \
+# /nethome/mct30/bmds/reads/BTIRed_Gut_trimmed_17-35_unmatched.fasta
+# # Builds new FASTA file for BTIRed gut reads that didn't perfectly
+# # align to reference genomes for Buchnera and/or Myzus
