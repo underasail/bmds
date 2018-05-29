23f90a6dd6c3ce35c269e3d4c276ded13b0f9ddf
underasail <underasail@yahoo.com>
2018-05-29 12:50:37 -0400

    Removed old code

--- aphid/G006-split-BSUB-pegasus.sh
+++ aphid/G006-split-BSUB-pegasus.sh
@@ -19,59 +19,11 @@
 module load bowtie2
 # Need to load bowtie2 module from /share
 
-##############
-# G006 Myzus #
-##############
 
-# Myzus index is the same as the one for G002 reads
-
-#/share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
-#-x /nethome/mct30/bmds/index/G006-Myzus/G006_Myzus_index \
-#-U /nethome/mct30/bmds/reads/G006_Bac_F_trimmed_17-35.fa \
-#-S /nethome/mct30/bmds/SAM_out/G006_Bac_Myzus-only.map
-# G006 bacteriocyte reads aligned against G006 Myzus
-
-#/share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
-#-x /nethome/mct30/bmds/index/G006-Myzus/G006_Myzus_index \
-#-U /nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35.fa \
-#-S /nethome/mct30/bmds/SAM_out/G006_Gut_Myzus-only.map
-# G006 gut reads aligned against G006 Myzus
-
-#################
-# G006 Buchnera #
-#################
-
-#/share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
-#-x /nethome/mct30/bmds/index/G006-Buchnera/G006_Buchnera_index \
-#-U /nethome/mct30/bmds/reads/G006_Bac_F_trimmed_17-35.fa \
-#-S /nethome/mct30/bmds/SAM_out/G006_Bac_Buchnera-only.map
-# G006 bacteriocyte reads aligned against G006 Buchnera (genome parts 1 and 2)
-
-#/share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
-#-x /nethome/mct30/bmds/index/G006-Buchnera/G006_Buchnera_index \
-#-U /nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35.fa \
-#-S /nethome/mct30/bmds/SAM_out/G006_Gut_Buchnera-only.map
-# G006 gut reads aligned against G006 Buchnera (genome parts 1 and 2)
-
-###############
-# G006 Plants #
-###############
-
-#/share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
-#-x /nethome/mct30/bmds/index/plants/plants_index \
-#-U /nethome/mct30/bmds/reads/G006_Bac_F_trimmed_17-35.fa \
-#-S /nethome/mct30/bmds/SAM_out/G006_Bac_plants-only.map
-# G006 bacteriocyte reads aligned against the plants
-
-#/share/apps/bowtie2/2.2.6/bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
-#-x /nethome/mct30/bmds/index/plants/plants_index \
-#-U /nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35.fa \
-#-S /nethome/mct30/bmds/SAM_out/G006_Gut_plants-only.map
-# G006 gut reads aligned against the plants
-
-############
-# Parallel #
-############
+############################################
+# G006 Gut and Bacteriocyte Alignments for #
+# Myzus, Buchnera, and Plant Individually  #
+############################################
 
 parallel --link -j 6 \
 'bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
