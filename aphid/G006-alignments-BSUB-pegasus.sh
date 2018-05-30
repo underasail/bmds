5b9ef8d76ab00b59fb0eb0df2c70a8a175c0f663
underasail <underasail@yahoo.com>
2018-05-30 17:31:49 -0400

    Updated

--- aphid/G006-alignments-BSUB-pegasus.sh
+++ aphid/G006-alignments-BSUB-pegasus.sh
@@ -25,18 +25,18 @@ module load bowtie2
 # Myzus, Buchnera, and Plant Individually  #
 ############################################
 
-# parallel --link -j 6 \
-# 'bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
-# -x /nethome/mct30/bmds/index/{1}/{1}_index \
-# -U /nethome/mct30/bmds/reads/{2} \
-# -S /nethome/mct30/bmds/SAM_out/{3}' \
-# ::: G006-Myzus G006-Myzus G006-Buchnera G006-Buchnera plants plants \
-# ::: G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
-# G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
-# G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
-# ::: G006_Bac_Myzus-only.map G006_Gut_Myzus-only.map \
-# G006_Bac_Buchnera-only.map G006_Gut_Buchnera-only.map \
-# G006_Bac_plants-only.map G006_Gut_plants-only.map
+parallel --link -j 6 \
+'bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
+-x /nethome/mct30/bmds/index/{1}/{1}_index \
+-U /nethome/mct30/bmds/reads/{2} \
+-S /nethome/mct30/bmds/SAM_out/{3}' \
+::: G006-Myzus G006-Myzus G006-Buchnera G006-Buchnera plants plants \
+::: G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
+G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
+G006_Bac_F_trimmed_17-35.fa G006_Gut_F_trimmed_17-35.fa \
+::: G006_Bac_Myzus-only.map G006_Gut_Myzus-only.map \
+G006_Bac_Buchnera-only.map G006_Gut_Buchnera-only.map \
+G006_Bac_plants-only.map G006_Gut_plants-only.map
 
 parallel --link -j 2 \
 'bowtie2 -L 10 -f -p 8 --no-unal --no-hd \
--- aphid/bowtie2-indexing-BSUB-pegasus.sh
+++ aphid/bowtie2-indexing-BSUB-pegasus.sh
@@ -3,7 +3,7 @@
 #BSUB -J bowtie2-indexing
 #BSUB -e /scratch/projects/acypi/err/bowtie2-indexing.err
 #BSUB -o /scratch/projects/acypi/out/bowtie2-indexing.out
-#BSUB -n 3
+#BSUB -n 1
 #BSUB -P acypi
 #BSUB -q bigmem
 #BSUB -R "rusage[mem=2000]"
@@ -19,23 +19,26 @@
 
 module load bowtie2
 
-#parallel --link \
-#'export filelist=
-#for filename in /nethome/mct30/bmds/ref_genomes/{}/*.fasta; do
-#    filelist+=$filename;
-#    filelist+=",";
-#done
-#
-#bowtie2-build -f \
-#$filelist \
-#/nethome/mct30/bmds/index/{}/{}_index' \
-#::: plants viruses other_bacteria G006-Myzus G006-Buchnera \
-#G002-Buchnera BTIRed-Buchnera
-
 parallel --link \
-"bowtie2-build -f \
-/nethome/mct30/bmds/ref_genomes/{}-Buchnera/{}_Buchnera_genome_ref.fasta,\
-/nethome/mct30/bmds/ref_genomes/G006-Myzus/G006_Myzus_genome_ref.fasta,\
-/nethome/mct30/bmds/ref_genomes/plants/GCA_000604025.1_BOL_v1.0_genomic.fasta \
-/nethome/mct30/bmds/index/{}/{}_Buchnera_Myzus_plant_index" \
-::: G002 G006 BTIRed
+'export filelist=
+for filename in /nethome/mct30/bmds/ref_genomes/{}/*.fasta; do
+    filelist+=$filename;
+    filelist+=",";
+done
+
+bowtie2-build -f \
+$filelist \
+/nethome/mct30/bmds/index/{}/{}_index' \
+::: plants 
+# viruses other_bacteria G006-Myzus G006-Buchnera \
+# G002-Buchnera BTIRed-Buchnera
+# # Add these lines back in for all at once. Commented out
+# # just for plant
+
+# parallel --link \
+# "bowtie2-build -f \
+# /nethome/mct30/bmds/ref_genomes/{}-Buchnera/{}_Buchnera_genome_ref.fasta,\
+# /nethome/mct30/bmds/ref_genomes/G006-Myzus/G006_Myzus_genome_ref.fasta,\
+# /nethome/mct30/bmds/ref_genomes/plants/GCF_000695525.1_BOL_genomic.fna \
+# /nethome/mct30/bmds/index/{}/{}_Buchnera_Myzus_plant_index" \
+# ::: G002 G006 BTIRed
