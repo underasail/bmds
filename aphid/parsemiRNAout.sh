007adb4523ae9ccd577e10c24180ed6e2c8b2804
underasail <underasail@yahoo.com>
2018-06-28 12:48:37 -0400

    Added plant parsing

--- aphid/parsemiRNAout.sh
+++ aphid/parsemiRNAout.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-while read -r a b c d e f g h i j k l m n o p q r s; do
+while read -r a b c d e f g h i j k l m n o p q; do
     echo -n '>'
     echo $a
     echo $p
-done < result_2.csv
+done < result_2.csv > result_2.fa
+
+# while read -r a b c d e f g h; do
+#      echo -n '>'
+#      echo $d
+#      echo $g
+# done < G006_Gut_plant_filter_P_prediction > G006_Gut_plant_filter_P_prediction.fa
