c75cfa6247dfb1d6ac61c2b4ee3a11b4255c4eb7
underasail <underasail@yahoo.com>
2018-09-24 12:40:03 -0500

    Added shell script to complement

---
+++ aphid/elimUnmatched_plant-only.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+~/gitclones/bmds/aphid/elimUnmatched_plant-only.py \
+~/bmds/SAM_out/G006_Gut_Myzus-only.map ~/bmds/SAM_out/G006_Gut_Buchnera-only.map \
+~/bmds/SAM_out/G006_Gut_plants-only.map ~/bmds/reads/G006_Gut_F_trimmed_17-35.fa \
+~/bmds/reads/G006_Gut_F_trimmed_17-35_plants-only2.fasta
