0d16793be1ccc7ec2406cd9a56be288bc52041af
underasail <underasail@yahoo.com>
2018-09-24 12:36:59 -0500

    Added script to collect plant-only subsection from fig 1

--- aphid/ShortStack.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-ShortStack \
---readfile /nethome/mct30/bmds/reads/G006_Gut_F_trimmed_17-35_plants.fasta \
---outdir /nethome/mct30/bmds/ShortStack_5X_17-35_inclusion \
---genomefile /nethome/mct30/bmds/ref_genomes/plants/GCF_000695525.1_BOL_genomic_inclusion.fasta \
---bowtie_cores 1 --mincov 10 --dicermin 17 --dicermax 35 --foldsize 700 --mmap f
-
-# /nethome/mct30/bmds/ref_genomes/plants/GCF_000695525.1_BOL_genomic_inclusion.fasta
